595f9e8dbf2e8cf4bf590d7ee0641aae2e3eb1a0
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-05-24 11:19:33 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from restaurante import Restaurante
+from Classes.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 restaurante_japones = Restaurante('Japa', 'Japonesa')
 
 restaurante_mexicano.alternar_estado()
-
+restaurante_praca.receber_avaliacao('gui',10)
+restaurante_praca.receber_avaliacao('mariana',5)
+restaurante_japones.receber_avaliacao('mariana',8)
 
 def main():
-    pass
+    Restaurante.listar_restaurantes()
 
 if __name__ == '__main__':
     main()
